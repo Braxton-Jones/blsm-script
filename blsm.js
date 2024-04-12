f9ca65f4f90192b76b48bab50c343274b26e53a4
Braxton <58635532+Braxton-Jones@users.noreply.github.com>
2024-04-12 05:22:09 -0400

    update script

--- blsm.js
+++ blsm.js
@@ -1,48 +1,55 @@
 const axios = require('axios');
 
-const commitMessage = process.env.COMMIT_MESSAGE;
-const commitRepo = process.env.COMMIT_REPO;
-const issueNumber = process.env.ISSUE_NUMBER;
-const issueTitle = process.env.ISSUE_TITLE;
-const issueBody = process.env.ISSUE_BODY;
-const issueRepo = process.env.ISSUE_REPO;
-const issueAction = process.env.ISSUE_ACTION;
-const prNumber = process.env.PR_NUMBER;
-const prTitle = process.env.PR_TITLE;
-const prBody = process.env.PR_BODY;
-const prRepo = process.env.PR_REPO;
-const prAction = process.env.PR_ACTION;
-const timestamp = process.env.TIMESTAMP;
-const eventType = process.env.TYPE;
-const username = process.env.USER;
-const repo = process.env.REPO;
+const EVENT_NAME = process.env.EVENT_NAME || '';
+const REPO = process.env.REPO || '';
+const REPO_OWNER = process.env.USERNAME || '';
+
+// Commit related variables
+const COMMIT_MSG = process.env.COMMIT_MSG || '';
+const COMMIT_TIMESTAMP = process.env.COMMIT_TIMESTAMP || '';
+const COMMIT_ID = process.env.COMMIT_ID || '';
+const COMMIT_URL = process.env.COMMIT_URL || '';
+
+// Pull request related variables
+const PULL_REQUEST_NUMBER = process.env.PULL_REQUEST_NUMBER || '';
+const PULL_REQUEST_STATE = process.env.PULL_REQUEST_STATE || '';
+const PULL_REQUEST_TITLE = process.env.PULL_REQUEST_TITLE || '';
+const PULL_REQUEST_BODY = process.env.PULL_REQUEST_BODY || '';
+
+// Issue related variables
+const ISSUE_ACTION = process.env.ISSUE_ACTION || '';
+const ISSUE_BODY = process.env.ISSUE_BODY || '';
+const ISSUE_TITLE = process.env.ISSUE_TITLE || '';
+const ISSUE_NUMBER = process.env.ISSUE_NUMBER || '';
+const ISSUE_STATE = process.env.ISSUE_STATE || '';
 
 
 const dataFromAction = {
-  type: eventType,
-  timestamp: timestamp,
-  repo: repo,
-  username: username,
+  type: EVENT_NAME,
+  timestamp: COMMIT_TIMESTAMP,
+  repoID: 9, // DO NOT CHANGE THIS, IF YOU DO, THE MICROSERVICE WILL NOT WORK AND YOU WILL NEED TO START OVER
+  username: REPO_OWNER,
+  repo: REPO,
   commitDetails: {
-    timestamp: timestamp,
-    message: commitMessage,
-    repo: commitRepo,
+    message: COMMIT_MSG,
+    timestamp: COMMIT_TIMESTAMP,
+    commitID: COMMIT_ID,
+    commitURL: COMMIT_URL,
   },
   pullRequestDetails: {
-    action: prAction,
-    timestamp: timestamp,
-    number: prNumber,
-    title: prTitle,
-    body: prBody,
-
+    number: PULL_REQUEST_NUMBER,
+    state: PULL_REQUEST_STATE,
+    title: PULL_REQUEST_TITLE,
+    body: PULL_REQUEST_BODY,
   },
   issueDetails: {
-    action: issueAction,
-    timestamp: timestamp,
-    number: issueNumber,
-    title: issueTitle,
-    body: issueBody,
+    action: ISSUE_ACTION,
+    body: ISSUE_BODY,
+    title: ISSUE_TITLE,
+    number: ISSUE_NUMBER,
+    state: ISSUE_STATE,
   },
+
 }
 
 async function sendToBackend(data) {
@@ -51,13 +58,14 @@ async function sendToBackend(data) {
     return;
   }
   const JSONdata = JSON.stringify(data);
-  console.log('Data from action:', dataFromAction);
-  console.log('Data to send:', JSONdata);
+  console.log("full data")
+  console.log(dataFromAction)
+  console.log("json data", JSONdata)
 
-  axios.post('http://localhost:3000/api/blsm', JSONdata)
+  axios.post('https://blossom-ai-rose.vercel.app/api/blsm', JSONdata)
     .then(response => {
       console.log('Getting response from microservice...');
-      console.log('Response from microservice:', response);
+      console.log('Response from microservice:', response.status, response.statusText);
     })
     .catch(error => {
       console.error('Error sending data:', error);
